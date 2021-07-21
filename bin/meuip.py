46bb6a6bb190f2b3a7e932977b1d08b3890d34da
andersonbispos <andersonbispos@gmail.com>
2021-07-21 15:24:30 -0300

    first script

---
+++ bin/meuip.py
@@ -0,0 +1,11 @@
+import requests
+#import json
+
+def meuipjson_requests():
+    url = 'https://ipinfo.io/json'
+    site = requests.get(url).json()
+    print(site['ip'])
+    # print(site['city'])
+
+if __name__ == '__main__':
+    meuipjson_requests()
